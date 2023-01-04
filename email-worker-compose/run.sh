c8fdb13dc10af3df4e26e31618594ea1545b9e63
Luís Gustavo <luisgsilva270@gmail.com>
2023-01-04 18:16:06 +0000

    update run.sh

--- email-worker-compose/run.sh
+++ email-worker-compose/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+docker-compose up -d
+docker-compose ps
+
+echo "Aguardando a carga do postgres"
+sleep 5
+
 # comando para listar os banco de dados
 docker-compose exec db psql -U postgres -c '\l'
+
+docker-compose down
