455ecbc9dd05bf4a6c7658fd5b44be73f97b1839
Luís Gustavo <luisgsilva270@gmail.com>
2023-01-10 14:53:12 +0000

    configurando variáveis de ambiente

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -2,11 +2,14 @@ import redis
 import json
 from time import sleep
 from random import randint
-
+import os
 
 if __name__ == '__main__':
-    r = redis.Redis(host='queue', port=6379, db=0)
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    r = redis.Redis(host=redis_host, port=6379, db=0)
+
     print('Aguardando mensagens ...')
+    
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         print('Mandando a mensagem:', mensagem['assunto'])
