e62b77377d6c6e436608a7098cb644b1188df8aa
Luís Gustavo <luisgsilva270@gmail.com>
2023-01-10 14:41:43 +0000

    configurado a opção de scale

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -6,6 +6,7 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Aguardando mensagens ...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         print('Mandando a mensagem:', mensagem['assunto'])
