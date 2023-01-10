c4d73b795634c4e577ee72ec1a0bb8774c649196
Luís Gustavo <luisgsilva270@gmail.com>
2023-01-10 14:15:34 +0000

    adicionado o redis

--- email-worker-compose/app/app.sh
+++ email-worker-compose/app/app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install bottle==0.12.13 psycopg2
+pip install bottle==0.12.13 psycopg2 redis==2.10.5
 python -u sender.py
--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,27 +1,40 @@
 import psycopg2
-from bottle import route, run, request
-
-DSN = 'dbname=email_sender user=postgres password=admin host=db'
-SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
-
-
-def register_message(assunto, mensagem):
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL, (assunto, mensagem))
-    conn.commit()
-    cur.close()
-    conn.close()
-    print('Mensagem registrada !')
+import redis
+import json
+import os
+from bottle import Bottle, request, run
 
 
-@route('/', method='POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
-    register_message(assunto, mensagem)
-    return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
-        assunto, mensagem)
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.fila = self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres password=admin host=db'
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        self.conn = psycopg2.connect(DSN)
+
+
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+
+        print('Mensagem registrada !')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+            assunto, mensagem
+        )
 
 
 if __name__ == '__main__':
